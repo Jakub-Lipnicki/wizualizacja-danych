a6cc53bebc9e7827c4a4af940cae46887a961a66
Jakub-Lip <jakublipnicki1@gmail.com>
2023-03-22 12:45:41 +0100

    zajecia 04 zad 4/6

---
+++ lab04.py
@@ -0,0 +1,52 @@
+# #zadanie 1:
+# liczby = [x*2 for x in range(0,31)]
+# print(liczby)
+#
+# plik = open("plik_zajecia.txt", "r+")
+# plik.writelines(str(liczby))
+# for line in plik:
+#     print(line)
+# plik.close()
+
+# #zadanie 2:
+# plik2 = open("plik_zajecia.txt", "r")
+# for line in plik2:
+#     print(line)
+# plik2.close()
+
+# #zadanie 3:
+# with open("plik_zajecia.txt", "r+") as plik:
+#     plik.write('ala ma kota1\n')
+#     plik.write('ala ma kota1\n')
+#     plik.write('ala ma kota1\n')
+#     for linia in plik:
+#         print(linia, end="")
+# #zadanie 4:
+#
+# class NaZakupy:
+#     def __init__(self, nazwa_produktu, ilosc, jednostka_miary, cena_jed):
+#         self.nazwa_produktu=nazwa_produktu
+#         self.ilosc=ilosc
+#         self.jednostka_miary=jednostka_miary
+#         self.cena_jed=cena_jed
+#     def wyswielt_produkt(self):
+#         return self.nazwa_produktu, self.ilosc, self.jednostka_miary, self.cena_jed
+#     def ile_produktu(self):
+#         return print(self.ilosc, self.jednostka_miary)
+#     def ile_kosztuje(self):
+#         return print("cena: ", self.ilosc*self.cena_jed)
+#
+# obiekt = NaZakupy("mleko", 5, "sztuk", 5)
+# print(obiekt.wyswielt_produkt())
+# print(obiekt.ile_produktu())
+# print(obiekt.ile_kosztuje())
+
+#zadanie 5:
+
+class Ciag:
+
+
+
+
+
+
