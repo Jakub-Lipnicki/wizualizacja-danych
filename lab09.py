69b3cbae72a8b18bcc54baf7a9349abfdb2e21c0
Jakub-Lip <jakublipnicki1@gmail.com>
2023-05-24 12:00:47 +0200

    5/5

--- lab09.py
+++ lab09.py
@@ -70,12 +70,42 @@ import seaborn as sns
 
 #wykres liniowy z wykorzystaniem ramki danych
 
+# sns.set()
+# df = pd.read_csv('iris.data', header=0, sep=',', decimal='.')
+# print(df)
+# wykres = sns.lineplot(data=df, x=df.index, y='sepal length', hue='class')
+# wykres.set_xlabel('indeksy')
+# wykres.set_title('Wykres liniowy danych z Iris dataset')
+# wykres.legend(title='Rodzaj kwiatu')
+# plt.show()
+
+#wykres punktowy
+
+# sns.set()
+# data = {'a': np.arange(10),'c': np.random.randint(0, 50, 10),'d': np.random.randn(10)}
+# data['b'] = data['a'] + 10 * np.random.randn(10)
+# data['d'] = np.abs(data['d']) * 100
+# print(data['c'])
+# print(data['d'])
+# df = pd.DataFrame(data)
+# plot = sns.relplot(data=df, x="a", y="b", hue="c", palette='bright', size="d", legend=True)
+# plot.fig.set_size_inches(6, 6)
+# plot.set(xticks=data['a'])
+# plt.show()
+
+#wykres kolumnowy
+data = {'Kraj': ['Belgia', 'Indie', 'Brazylia', 'Polska'],
+        'Stolica': ['Bruksela', 'New Delhi', 'Brasilia', 'Warszawa'],
+        'Kontynent': ['Europa', 'Azja', 'AmerykaPołudniowa', 'Europa'],
+        'Populacja': [11190846, 1303171035, 207847528, 38675467]}
+df = pd.DataFrame(data)
 sns.set()
-df = pd.read_csv('iris.data', header=0, sep=',', decimal='.')
-print(df)
-wykres = sns.lineplot(data=df, x=df.index, y='sepal length', hue='class')
-wykres.set_xlabel('indeksy')
-wykres.set_title('Wykres liniowy danych z Iris dataset')
-wykres.legend(title='Rodzaj kwiatu')
+#plot = sns.catplot(data=df, x='Kontynent', y='Populacja', kind='bar', ci=None, hue='Kontynent', estimator=np.sum,#                    dodge=False, palette=['red', 'green', 'yellow'], legend_out=False)# plot.fig.set_size_inches(7, 6)# plot.add_legend(title='Populacja na kontynentach', loc='upper right')# plot.fig.suptitle('Populacja na kontynentach')
+plot = sns.barplot(data=df, x='Kontynent', y='Populacja',
+                   ci=None, hue='Kontynent', estimator=np.sum,
+                   dodge=False, palette=['red', 'green', 'yellow'])
+plot.legend(title='Populacja na kontynentach')
+plot.set(title='Wykres słupkowy')
 plt.show()
+
 
