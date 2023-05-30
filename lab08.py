64b05a08b6d1e3053ad699e217d6137b67007a62
Jakub-Lip <jakublipnicki1@gmail.com>
2023-05-30 20:10:47 +0200

    finished

--- lab08.py
+++ lab08.py
@@ -145,5 +145,143 @@ y2 = np.cos(x2)
 # plt.show()
 #
 
+#PROSTY WYKRES LINIOWY
+# plt.plot([1,2,0,3,4,30])
+# plt.ylabel('jakieś liczby')
+# plt.show()
+
+#przekazujemy dwa wektory wartości, najpierw dla wektora x,
+#następnie dla wektora y
+#dodatkowo mamy tutaj przekazywany parametr w postaci stringa,
+#który określa styl wykresu
+# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro-')
+# #tutaj określamy listę parametrów w postaci [xmin, xmax, ymin, ymax]
+# plt.axis([0, 6, 0, 20])
+# plt.show()
+#
+# #możemy też ustawić różne kolory dla poszczególnych elementów nakładając na siebie dwa wykresy
+# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'r')
+# plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'o')
+# plt.axis([0, 6, 0, 20])
+# plt.show()
+#
+# #bazowy wektor wartości
+# t = np.arange(0., 5., 0.2)
+# #za pomocą pojedynczego wywołania funkcji plot() możemy
+# #wygenerować wiele wykresów na jednym płótnie (ang. canvas)
+# #każdorazowo podając niezbędne wartości: wartości dla osi x,
+# #wartości dla osi y, styl wykresu, ...
+# plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+# plt.legend(labels=['liniowa', 'kwadratowa', 'szescienna'])
+# plt.show()
+#
+#
+# x = np.linspace(0, 2, 100)
+# #wykresy mogą być też dodawane do płótna definicja po definicji zamiast w pojedynczym wywołaniu funkcji
+# #plot()
+# #tutaj użyty został również parametr label, który
+# #określa etykietę danego wykresu w legendzie
+# plt.plot(x, x, label="liniowa")
+# plt.plot(x, x**2, label="kwadratowa")
+# plt.plot(x, x**3, label="sześcienna")
+# #etykiety osi
+# plt.xlabel('etykieta x')
+# plt.ylabel("etykieta y")
+# #tytuł wykresu
+# plt.title("Prosty wykres")
+# #włączamy pokazanie legendy
+# plt.legend()
+# plt.savefig('wykres matplot.png')
+# plt.show()
+# im1 = Image.open('wykres matplot.png')
+# im1 = im1.convert('RGB')
+# im1.save('nowy.jpg')
+
+#zad1
+# Na wykresie wyświetl wykres liniowy funkcji f(x) = 1/x dla x ϵ [1, 20]. Dodaj etykietę do linii wykresu i
+# wyświetl legendę. Dodaj odpowiednie etykiety do osi wykresu (‘x’, ’f(x)’) oraz ustaw zakres osi na (0,
+# 1) oraz (1, długość wektora x).
+x = np.arange(1, 21, 1)
+plt.plot(x, 1/x, label='f(x) = 1/x')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.axis([0, 20, 0, 1])
+plt.legend()
+plt.title('Wykres funkcji f(x) = 1/x dla x[1,20]')
+plt.show()
+# zad2
+#zmodyfikuj wykres z zad 1 tak aby wygladal jak na zrzucie z ekranu
+x = np.arange(1, 21, 1)
+plt.plot(x, 1/x, 'g>:', label='f(x) = 1/x' )
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.axis([0, 20, 0, 1])
+plt.legend()
+plt.title('Wykres funkcji f(x) = 1/x dla x[1,20]')
+plt.show()
+# zad3
+#Na jednym wykresie wygeneruj wykresy funkcji sin(x) oraz cos(x) dla x ϵ [0, 30] z krokiem 0.1.
+# Dodaj etykiety i legendę do wykresu.
+x = np.arange(0, 30, .1)
+plt.plot(x, np.sin(x), 'b', label='sin(x)')
+plt.plot(x, np.cos(x), 'r', label='cos(x)')
+plt.xlabel('x')
+plt.ylabel('sin(x) cos(x)')
+plt.legend(loc='upper right')
+plt.title('Wykres sin(x), cos(x)')
+plt.show()
+# zad4
+#Korzystając ze zbioru danych Iris (https://archive.ics.uci.edu/ml/datasets/iris) wygeneruj wykres
+# punktowy, gdzie wektor x to wartość ‘sepal length’ a y to ‘sepal width’, dodaj paletę kolorów c na
+# przykładzie listingu 6 a parametr s niech będzie wartością absolutną z różnicy wartości
+# poszczególnych elementów wektorów x oraz y.
+df = pd.read_csv('iris.data', header=0, sep=',', decimal='.')
+print(df)
+# przygotowanie wektora kolorów
+colors = np.random.randint(0, 50, len(df.index))
+# przygotowanie wektora z rozmiarami 'kropek'
+scale = np.abs(df['sepal length'] - df['sepal width'])
+plt.scatter(df['sepal length'], df['sepal width'], c=colors, s=scale)
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.show()
+#zad5
+#Korzystając z biblioteki pandas wczytaj zbiór danych z narodzinami dzieci przedstawiony w lekcji 8.
+# Następnie na jednym płótnie wyświetl 3 wykresy (jeden wiersz i 3 kolumny). Dodaj do wykresów
+# stosowne etykiety. Poustawiaj różne kolory dla wykresów.
+# 1 wykres – wykres słupkowy przedstawiający ilość narodzonych dziewczynek i chłopców w całym
+# okresie.
+# 2 wykres – wykres liniowy, gdzie będą dwie linie, jedna dla ilości urodzonych kobiet, druga dla
+# mężczyzn dla każdego roku z osobna. Czyli y to ilość narodzonych kobiet lub mężczyzn (dwie linie), x
+# to rok.
+# 3 wykres – wykres słupkowy przedstawiający sumę urodzonych dzieci w każdym roku
+xlsx = pd.ExcelFile('imiona.xlsx')
+df = pd.read_excel(xlsx, header=0)
+print(df)
+plt.subplot(1, 3, 1)
+grouped = df.groupby('Plec')
+etykiety = list(grouped.groups.keys())
+wartosci = list(grouped.agg('Liczba').sum())
+plt.bar(x=etykiety, height=wartosci, color=['green', 'red'])
+plt.xlabel('Płeć')
+plt.ylabel('Liczba narodzonych dzieci')
+# wykres 2
+plt.subplot(1, 3, 2)
+x = df['Rok'].unique()
+kobiety = df[(df.Plec == 'K')].groupby('Rok').agg({'Liczba':['sum']}).values
+mezczyzni = df[(df.Plec == 'M')].groupby('Rok').agg({'Liczba':['sum']}).values
+plt.plot(x, kobiety, label="Kobiety")
+plt.plot(x, mezczyzni, label="Mężczyźni")
+plt.xlabel('Rok')
+#
+# # wykres 3
+plt.subplot(1, 3, 3)
+x = df['Rok'].unique()
+y = df.groupby('Rok').agg('Liczba').sum()
+plt.bar(x, y)
+plt.xlabel('Rok')
+# wyświetlamy cały wykres
+plt.subplots_adjust(wspace=0.85)
+plt.show()
 
 
