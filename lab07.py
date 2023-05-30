460109b07af670064a2c5db5f1ba7d4a3165abe3
Jakub-Lip <jakublipnicki1@gmail.com>
2023-05-30 20:20:34 +0200

    finished

--- lab07.py
+++ lab07.py
@@ -69,20 +69,73 @@ df = pd.read_excel(xlsx, header=0)
 print(df)
 
 # zad 2
+print(df[df.Liczba > 1000])
+print('')
+print(df[df.Imie == 'JAKUB'])
+print('')
+print(df.Liczba.sum())
+print('')
+grupa = df[df.Rok < 2006]
+print(grupa.Liczba.sum())
+grupa = df[df.Rok < 2006].groupby('Rok').agg({'Liczba': ['sum']})
+print(grupa)
+print('')
+print('')
+print("Chłopiec")
+print(df[df['Plec'] == 'M'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+                                                                                   ascending=False).iloc[0])
+print("Dziewczynka")
+print(df[df['Plec'] == 'K'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+                                                                                   ascending=False).iloc[0])
+
+print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).nth(0))
+# print(df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec']).first())
+
+new_df = df.sort_values('Liczba', ascending=False).groupby(['Rok', 'Plec'])
+for index, group in enumerate(new_df, start=1):
+    print(f"{index} {group[0]}")
+    print(f" {group[1].iloc[0]['Imie']}", end='')
+    print(f" {group[1].iloc[0]['Liczba']}")
+print('')
+#ZAD3
+df = pd.read_csv('zamowienia.csv', header=0, sep=';', decimal='.')
+print(df)
+print(df['Sprzedawca'].unique())
+print('')
+print(df.sort_values('Utarg', ascending=False).head(5))
+print('')
+print(df.groupby('Sprzedawca').size())
+print('')
+print(df.groupby('Kraj').agg({'Utarg': ['sum']}))
+print(df.groupby('Kraj').size())
+print('')
+print(df[(df['Kraj'] == 'Polska') & (df['Data zamowienia'] >= '2005-01-01') & (df['Data zamowienia'] <= '2005-12-31')].
+      agg({'Utarg': ['sum']}))
+print('')
+print(df[df['Data zamowienia'].str[:4] == '2004'].agg({'Utarg': ['mean']}))
+rok_2004 = df[((df[ 'Data zamowienia'] >= '2004-01-01') & (df['Data zamowienia'] <= '2004-12-31'))]
+rok_2004.to_csv("zamowienia_2004.csv", index=False)
+
+## print("Chłopiec")
+## print(df[df['Plec'] == 'M'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+##                                                                                    ascending=False).iloc[0])
+## print("Dziewczynka")
+## print(df[df['Plec'] == 'K'].groupby(['Imie']).agg({'Liczba': ['sum']}).sort_values(('Liczba', 'sum'),
+#                                                                                    ascending=False).iloc[0])
 # print(df[df['Liczba']>100])
-
+#
 # print(df[df['Imie']=='JAKUB'])
-
-#print(df.loc[df['Rok'] == 2001, 'Liczba'].sum())
-
+#
+# print(df.loc[df['Rok'] == 2001, 'Liczba'].sum())
+#
 # a = df.loc[(df['Rok'] == 2002), 'Liczba'].sum()
 # b = df.loc[(df['Rok'] == 2001), 'Liczba'].sum()
 # print(a+b)
-
+#
 # print("Chlopcy: ",df.loc[(df['Plec'] == 'M'), 'Liczba'].sum())
 # print("Dziewoje: ",df.loc[(df['Plec'] == 'K'), 'Liczba'].sum())
-
-print(df[df['Imie']=='JAKUB'])
+#
+# print(df[df['Imie']=='JAKUB'])
 
 
 
